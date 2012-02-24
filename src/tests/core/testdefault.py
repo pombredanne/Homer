3114e2c68deeada1e35d87e4edf5086b9bb8a428
iroiso <iroiso@live.com>
2012-02-24 09:04:06 -0500

    Added other tests for the Default descriptor

--- src/tests/core/testdefault.py
+++ src/tests/core/testdefault.py
@@ -9,7 +9,8 @@ Description:
 Unittests for the Default Property...
 """
 from unittest import TestCase,expectedFailure,skip
-from homer.core.models import Default
+from homer.core.models import Default, Model, key
+from homer.core.commons import String, Integer
     
 class TestDefault(TestCase):
     '''Does Default work as I expect'''
@@ -27,4 +28,24 @@ class TestDefault(TestCase):
         values = self.sample.default
         self.assertTrue(values)
         self.assertTrue(len(values) == 2)
+        
+
+    def testOtherDefaultStyle(self):
+        '''Tests the other default style'''
+        @key("name")
+        class SecondStyle(Model):
+            '''Second pattern of defaults'''
+            name = String()
+            @property
+            def default(self):
+                '''other style'''
+                return String, Integer
+
+        sample = SecondStyle()
+        self.assertTrue(len(sample.default) == 2)
+        self.assertRaises(AttributeError, lambda : delattr(self.sample, "default"))
+        self.assertRaises(AttributeError, lambda : setattr(self.sample, "default", "Hello"))
+        values = sample.default
+        self.assertTrue(values)
+        
         
